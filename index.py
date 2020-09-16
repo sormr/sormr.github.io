0cf8f96928beb499cdeb58365003fcf641785b9d
sormr <66499411+sormr@users.noreply.github.com>
2020-09-16 17:05:09 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-print('hello world!')
+print('''<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>sormr</title>
+  </head>
+  <body>
+      blank
+  </body>
+</html>''')
